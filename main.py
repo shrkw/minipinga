bba44ec8cd84fec12e9c5697c20f9112e4617527
shrkw <shrkwh@gmail.com>
2019-04-20 14:24:26 +0900

    implemented for each sites

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
-import requests
+from urllib.parse import urlparse
+
 import lxml.html
+import requests
 
 
-def main():
-    url = "https://comic-meteor.jp/idrunk/"
+def hostname(url: str) -> str:
+    return urlparse(url).hostname
+
+
+giga_viewer = "//span[@class='series-episode-list-date']"
+xpath_dict = {
+    "comic-meteor.jp": "//div[@class='latest_info_date']",
+    "comic-boost.com": "//div[@class='product_item']/p[@class='date']",
+    "web-ace.jp": "//span[@class='updated-date']",
+    "pocket.shonenmagazine.com": giga_viewer,
+    "kuragebunch.com": giga_viewer,
+    "tonarinoyj.jp": giga_viewer,
+}
+
+
+def main(opt):
+    url = "https://comic-boost.com/series/86"
+
     r = requests.get(url)
     html = lxml.html.fromstring(r.content)
-    htmltag = html.xpath("//div[@class='latest_info_date']")
-
-    for tag in htmltag:
-        print(tag.text)
+    if hostname(url) in xpath_dict.keys():
+        htmltag = html.xpath(xpath_dict[hostname(url)])
+        if htmltag is not None:
+            print(htmltag[0].text)
 
 
 if __name__ == "__main__":
-    main()
-
+    main({})
