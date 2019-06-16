0f9ec13dd3123a07d62dca681942d3c0ee6e408e
shrkw <shrkwh@gmail.com>
2019-06-17 01:02:52 +0900

    use firestore

--- main.py
+++ main.py
@@ -1,88 +1,56 @@
 import dataclasses
 import datetime
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, List, Optional
 from urllib.parse import urlparse
 
 import lxml.html
 import requests
+from google.cloud import firestore
 
 import notifier
+
+db = firestore.Client()
 
 
 def hostname(url: str) -> str:
     return urlparse(url).hostname
 
 
-@dataclasses.dataclass
-class MangaSite:
-    url: str
-    indication_text_xpath: str
-    has_feed: bool
+def save(id: str, indication_text_value: str) -> None:
+    doc_ref = db.collection("subscriptions").document(id)
+    doc_ref.update({"indication_text_value": indication_text_value})
 
 
-manga_sites: Dict[str, MangaSite] = {
-    "comic-meteor.jp": MangaSite(
-        "comic-meteor.jp", "//div[@class='latest_info_date']", False
-    ),
-    "comic-boost.com": MangaSite(
-        "comic-boost.com", "//div[@class='product_item']/p[@class='date']", False
-    ),
-    "web-ace.jp": MangaSite("web-ace.jp", "//span[@class='updated-date']", False),
-    "pocket.shonenmagazine.com": MangaSite("pocket.shonenmagazine.com", "", True),
-    "kuragebunch.com": MangaSite("kuragebunch.com", "", True),
-    "tonarinoyj.jp": MangaSite("tonarinoyj.jp", "", True),
-}
-
-
-@dataclasses.dataclass
-class Subscription:
-    url: str
-    title: str
-    last_checked_date: datetime.datetime
-    indication_text_value: str
-
-
-subscriptions: List[Subscription] = [
-    Subscription(
-        "https://comic-boost.com/series/86", "ken", datetime.datetime.today(), "a"
-    ),
-    Subscription(
-        "https://comic-meteor.jp/idrunk/", "idrunk", datetime.datetime.today(), "a"
-    ),
-    Subscription(
-        "https://web-ace.jp/youngaceup/contents/1000118/",
-        "FGO seraph",
-        datetime.datetime.today(),
-        "a",
-    ),
-]
-
-
-def save(url: str, text: str):
-    pass
-
-
-def check_update(sub: Subscription) -> Tuple[bool, Optional[str]]:
-    r = requests.get(sub.url)
+def check_update(sub: Dict) -> Optional[str]:
+    url = sub["url"]
+    r = requests.get(url)
     html = lxml.html.fromstring(r.content)
-    host = hostname(sub.url)
-    if host in manga_sites.keys():
-        element = html.xpath(manga_sites[host].indication_text_xpath)
+    host = hostname(url)
+
+    doc_ref = db.collection("manga_sites").document(host)
+    doc = doc_ref.get()
+    if doc.to_dict() is not None:
+        element = html.xpath(doc.to_dict()["indication_text_xpath"])
         if len(element) == 0:
             raise AssertionError("elements for updated_date are not found", sub)
         text = element[0].text
-        if text != sub.indication_text_value:
-            return (True, text)
-        return (False, None)
+        if text != sub["indication_text_value"]:
+            return text
+        return None
     else:
         raise AssertionError("unknown host!", sub)
 
 
-def main(opt):
-    for sub in subscriptions:
-        updated, indication_text_value = check_update(sub)
-        if updated:
-            notifier.notify(f"{sub.title} が {indication_text_value} に更新されている可能性があります {sub.url}")
+def main(opt) -> None:
+    subs_ref = db.collection("subscriptions")
+    for sub in subs_ref.stream():
+        sub_dict = sub.to_dict()
+        indication_text_value = check_update(sub_dict)
+        if indication_text_value:
+            notifier.notify(
+                f"{sub_dict['title']} が {indication_text_value} に更新されている可能性があります {sub_dict['url']}"
+            )
+            save(sub.id, indication_text_value)
 
 
 if __name__ == "__main__":
