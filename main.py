a9c3b5453d33b3a563a5968b6272b95ad67acc04
shrkw <shrkwh@gmail.com>
2019-10-07 00:06:28 +0900

    able to parse json

--- main.py
+++ main.py
@@ -26,13 +26,33 @@ def save(id: str, indication_text_value: str) -> None:
     )
 
 
+def dparse(dic: Dict, query: str, sep: str = "/", default=None) -> Optional[str]:
+    words = query.split(sep)
+
+    def _(dic_, words_, default):
+        if len(words_) == 0:
+            return default
+        if len(words_) == 1:
+            return dic_.get(words_[0], default)
+        else:
+            return _(dic_.get(words_[0], {}), words_[1:], default)
+
+    return _(dic, words, default=None)
+
+
 def get_indicator(manga_site, content):
-    html = lxml.html.fromstring(content)
-    element = html.xpath(manga_site["indication_text_xpath"])
-    if len(element) == 0:
-        raise AssertionError("elements for updated_date are not found")
-    text = element[0].text
-    return text
+    style = manga_site.get("style", "html")
+    if style == "json":
+        import json
+
+        js = json.loads(content)
+        return dparse(js, manga_site["indication_text_xpath"])
+    else:
+        html = lxml.html.fromstring(content)
+        element = html.xpath(manga_site["indication_text_xpath"])
+        if len(element) == 0:
+            raise AssertionError("elements for updated_date are not found")
+        return element[0].text
 
 
 def check_update(sub: Dict) -> Optional[str]:
